612398e601abc3e8a80df6eba12498e284c7780d
John Milmine <john.milmine@dna.co.nz>
2014-09-11 10:59:39 +1200

    moved this to yml

--- _config.php
+++ _config.php
@@ -1,7 +1,1 @@
 <?php
-
-// To add simple address fields to an object, use
-Object::add_extension('ElementContact', 'Addressable');
-
-// To add automatic geocoding to an object with the Addressable extension
-Object::add_extension('ElementContact', 'Geocodable');
