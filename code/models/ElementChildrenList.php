61c31f57d057a23d6cba9dc165c0b98f8ec670bb
Will Rossiter <will@fullscreen.io>
2015-01-19 10:29:19 +1300

    Add customizable sorting for child list.

--- code/models/ElementChildrenList.php
+++ code/models/ElementChildrenList.php
@@ -2,11 +2,21 @@
 
 class ElementChildrenList extends BaseElement {
 
-	private static $db = array();
+	private static $db = array(
+		'SortString' => 'Varchar(100)'
+	);
 
 	private static $has_one = array(
 		'ParentPage' => 'SiteTree'
 	);
 
 	private static $title = "Show a list of pages in a list.";
+
+	public function getChildrenList() {
+		if($page = $this->ParentPage()) {
+			return $page->AllChildren()->sort($this->SortString);
+		}
+
+		return null;
+	}
 }
