a1380196feec7968e3b451e094e9c36f4931366f
John Milmine <john.milmine@dna.co.nz>
2017-07-03 08:51:05 +1200

    moved to satellite modules

--- src/Extensions/ElementalPageExtension.php
+++ src/Extensions/ElementalPageExtension.php
@@ -318,27 +318,6 @@ class ElementalPageExtension extends DataExtension
     }
 
     /**
-     * If the page is duplicated across subsites, copy the elements across too.
-     *
-     * @return Page The duplicated page
-     */
-    public function onAfterDuplicateToSubsite($originalPage)
-    {
-        $originalElementalArea = $originalPage->getComponent('ElementalArea');
-        $duplicateElementalArea = $originalElementalArea->duplicate(false);
-        $duplicateElementalArea->write();
-        $this->owner->ElementalAreaID = $duplicateElementalArea->ID;
-        $this->owner->write();
-
-        foreach ($originalElementalArea->Items() as $originalElement) {
-            $duplicateElement = $originalElement->duplicate(true);
-
-            // manually set the ParentID of each element, so we don't get versioning issues
-            DB::query(sprintf("UPDATE Element SET ParentID = %d WHERE ID = %d", $duplicateElementalArea->ID, $duplicateElement->ID));
-        }
-    }
-
-    /**
      * Publish
      */
     public function onAfterPublish()
