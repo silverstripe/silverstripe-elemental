a5f4eb152da8089e3f53827554e56ae4d768da7c
John Milmine <john.milmine@dna.co.nz>
2017-07-14 23:43:28 +1200

    fixing table name

--- src/Tasks/ResetGloballyEnabledElements.php
+++ src/Tasks/ResetGloballyEnabledElements.php
@@ -18,18 +18,18 @@ class ResetGloballyEnabledElements extends BuildTask
         $default = Config::inst()->get('BaseElement', 'default_global_elements') ? 1 : 0;
 
         // first update all to the default
-        DB::query("UPDATE BaseElement SET AvailableGlobally = $default");
-        DB::query("UPDATE BaseElement_Live SET AvailableGlobally = $default");
-        DB::query("UPDATE BaseElement_versions SET AvailableGlobally = $default");
+        DB::query("UPDATE Element SET AvailableGlobally = $default");
+        DB::query("UPDATE Element_Live SET AvailableGlobally = $default");
+        DB::query("UPDATE Element_versions SET AvailableGlobally = $default");
 
         foreach($elementClasses as $class) {
             $isGlobal = Config::inst()->get($class, 'default_global_elements') ? 1 : 0;
             $ids = $class::get()->getIDList();
             if(!empty($ids)) {
                 $idStr = implode("','", $ids);
-                DB::query("UPDATE BaseElement SET AvailableGlobally = $isGlobal WHERE ID IN ('$idStr')");
-                DB::query("UPDATE BaseElement_Live SET AvailableGlobally = $isGlobal WHERE ID IN ('$idStr')");
-                DB::query("UPDATE BaseElement_versions SET AvailableGlobally = $isGlobal WHERE RecordID IN ('$idStr')");
+                DB::query("UPDATE Element SET AvailableGlobally = $isGlobal WHERE ID IN ('$idStr')");
+                DB::query("UPDATE Element_Live SET AvailableGlobally = $isGlobal WHERE ID IN ('$idStr')");
+                DB::query("UPDATE Element_versions SET AvailableGlobally = $isGlobal WHERE RecordID IN ('$idStr')");
             }
         }
     }
