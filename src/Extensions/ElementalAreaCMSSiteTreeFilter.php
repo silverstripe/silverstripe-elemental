ab83f9c2a3f1d508c591aa2222393aaf26fdf17b
Julian Thomson <julian.thomson@dna.co.nz>
2017-07-28 16:17:12 +1200

    Allow ElementArea content to be searchable using CMS page search form

---
+++ src/Extensions/ElementalAreaCMSSiteTreeFilter.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace DNADesign\Elemental\Controllers;
+
+use DNADesign\Elemental\Models\ElementalArea;
+use SilverStripe\CMS\Controllers\CMSSiteTreeFilter;
+use SilverStripe\Forms\DateField;
+
+class ElementalAreaCMSSiteTreeFilter extends CMSSiteTreeFilter {
+
+
+	public static function title()
+	{
+		return _t('DNADesign\\Elemental\\Controllers\\CMSSiteTreeFilter_Search.Title', "All Elemental content");
+	}
+
+	public function getFilteredPages()
+	{
+		$elements = ElementalArea::get();
+		$elements = $this->applyDefaultFilters($elements);
+
+		return $elements;
+	}
+
+	protected function applyDefaultFilters($query)
+	{
+		$sng = ElementalArea::singleton();
+		foreach ($this->params as $name => $val) {
+			if (empty($val)) {
+				continue;
+			}
+			switch ($name) {
+				case 'Term':
+
+					$query = $query->filterAny(array(
+						'SearchContent:PartialMatch' => $val
+					));
+					break;
+
+				case 'LastEditedFrom':
+					$fromDate = new DateField(null, null, $val);
+					$query = $query->filter("LastEdited:GreaterThanOrEqual", $fromDate->dataValue().' 00:00:00');
+					break;
+
+				case 'LastEditedTo':
+					$toDate = new DateField(null, null, $val);
+					$query = $query->filter("LastEdited:LessThanOrEqual", $toDate->dataValue().' 23:59:59');
+					break;
+
+				case 'ClassName':
+					if ($val != 'All') {
+						$query = $query->filter('ClassName', $val);
+					}
+					break;
+
+				default:
+					$field = $sng->dbObject($name);
+
+					if ($field) {
+						$filter = $field->defaultSearchFilter();
+						$filter->setValue($val);
+						$query = $query->alterDataQuery(array($filter, 'apply'));
+					}
+			}
+		}
+		return $query;
+	}
+
+}
