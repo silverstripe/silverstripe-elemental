cfbd98457cc8df2c488b63f2a2de57a3e0509906
Dylan Wagstaff <dylan@silverstripe.com>
2018-04-24 02:31:13 +0000

    NEW add ElementalArea field type
    Which subclasses GridField and behaves with no altered functionality
    but upon a read only transformation renders out it's associated Elements
    (the items in the grid field). By Render out here this means that a
    summary of sorts by the way of the 'Root.Main' tab of said element's
    getCMSFields.
    
    This in turns allows a page to feature history complete with a content
    summary when inspecting a version, since this module does away with
    the default Content field.

---
+++ src/Forms/ElementalAreaField.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace DNADesign\Elemental\Forms;
+
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\CompositeField;
+use SilverStripe\Forms\FieldGroup;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TabSet;
+use SilverStripe\Forms\ReadonlyTransformation;
+
+class ElementalAreaField extends GridField
+{
+    public function performReadonlyTransformation()
+    {
+        $readOnlyField = $this->castedCopy(CompositeField::class);
+        
+        $blockReducer = function ($element) {
+            $parentName = 'Element' . $element->ID;
+            $elementFields = $element->getCMSFields();
+            // Obtain highest impact fields for a summary (e.g. Title & Content)
+            foreach ($elementFields as $field) {
+                if (is_object($field) && $field instanceof TabSet) {
+                    // Assign the fields of the first Tab in the TabSet - most regularly 'Root.Main'
+                    $elementFields = $field->FieldList()->first()->FieldList();
+                    break;
+                }
+            }
+            // Set values (before names don't match anymore)
+            $elementFields->setValues($element->getQueriedDatabaseFields());
+            // Ensure field names are unique between elements on parent form
+            $elementFields->recursiveWalk(function ($field) use ($parentName) {
+                $field->setName($parentName . '_' . $field->getName());
+            });
+            // Combine into an appropriately named group
+            $elementGroup = FieldGroup::create($elementFields);
+            $elementGroup->setName($parentName);
+			$elementGroup->addExtraClass('elementalarea-element');
+            
+            return $elementGroup;
+        };
+        
+        $readOnlyField->setChildren(
+            FieldList::create(
+                array_map($blockReducer, $this->getList()->toArray())
+            )
+        );
+        $readOnlyField = $readOnlyField->transform(new ReadonlyTransformation());
+        $readOnlyField->setReadOnly(true);
+        $readOnlyField->setName($this->getName());
+		$readOnlyField->addExtraClass('elementalarea--read-only');
+        return $readOnlyField;
+    }
+}
