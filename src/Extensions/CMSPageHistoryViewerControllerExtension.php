7a3cb47dfb850c21d3ff4b1fbb4725440a4857d6
Robbie Averill <robbie@silverstripe.com>
2018-05-15 16:43:53 +1200

    NEW Add extension to activate history viewer when blocks pages are being viewed

---
+++ src/Extensions/CMSPageHistoryViewerControllerExtension.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DNADesign\Elemental\Extensions;
+
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Core\Extension;
+
+/**
+ * Instructs the history viewer controller to be enabled for pages that have elemental
+ */
+class CMSPageHistoryViewerControllerExtension extends Extension
+{
+    public function updateIsEnabled(SiteTree $page = null)
+    {
+        return $page && $page->hasExtension(ElementalPageExtension::class);
+    }
+}
