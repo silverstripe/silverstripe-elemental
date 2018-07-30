ad9a035b6d0a0e15a8f9a4b77cfdc7af59f03c90
Guy Marriott <guy@scopey.co.nz>
2018-07-30 15:24:20 +1200

    Refactor elemental editor to extend FormField and setup a template

--- src/ElementalEditor.php
+++ src/ElementalEditor.php
@@ -3,33 +3,14 @@
 namespace DNADesign\Elemental;
 
 use DNADesign\Elemental\Models\ElementalArea;
-use SilverStripe\Core\Extensible;
-use SilverStripe\Core\Injector\Injectable;
 use SilverStripe\Forms\FormField;
-use SilverStripe\Forms\LiteralField;
 
-class ElementalEditor
+class ElementalEditor extends FormField
 {
-    use Extensible;
-    use Injectable;
-
     /**
      * @var ElementalArea $area
      */
     protected $area;
-
-    /**
-     * @var string $name
-     */
-    protected $name;
-
-    /**
-     * By default, no need for a title on the editor. If there is more than one
-     * area then use `setTitle` to describe.
-     *
-     * @var string $title
-     */
-    protected $title = '';
 
     /**
      * @var array $type
@@ -42,8 +23,11 @@ class ElementalEditor
      */
     public function __construct($name, ElementalArea $area)
     {
-        $this->name = $name;
+        // By default, no need for a title on the editor. If there is more than one area then use `setTitle` to describe
+        parent::__construct($name, '');
         $this->area = $area;
+
+        $this->addExtraClass('element-editor__container');
     }
 
     /**
@@ -76,25 +60,5 @@ class ElementalEditor
     public function getArea()
     {
         return $this->area;
-    }
-
-    /**
-     * @param string $title
-     *
-     * @return $this
-     */
-    public function setTitle($title)
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    /**
-     * @return FormField
-     */
-    public function getField()
-    {
-        return LiteralField::create($this->name, '<div class="element-editor__container"></div>');
     }
 }
---
+++ templates/DNADesign/Elemental/ElementalEditor.ss
@@ -0,0 +1,3 @@
+<div $HTTPAttributes>
+
+</div>
