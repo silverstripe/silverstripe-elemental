e787c460cdb1742bf9e01a3ab154c96c958dcd16
Guy Marriott <guy@scopey.co.nz>
2018-07-30 16:00:13 +1200

    Updating test considering recently removed functionality

--- tests/ElementalEditorTest.php
+++ tests/ElementalEditorTest.php
@@ -8,6 +8,7 @@ use DNADesign\Elemental\Models\ElementalArea;
 use DNADesign\Elemental\Models\ElementContent;
 use Page;
 use SilverStripe\Dev\SapphireTest;
+use SilverStripe\Forms\FormField;
 use SilverStripe\Forms\GridField\GridField;
 use Symbiote\GridFieldExtensions\GridFieldAddNewMultiClass;
 
@@ -24,22 +25,9 @@ class ElementalEditorTest extends SapphireTest
     public function testGetField()
     {
         $area = $this->objFromFixture(ElementalArea::class, 'area1');
-        $editor = ElementalEditor::create('ElementalArea', $area);
 
-        //
-        $field = $editor->getField();
-        $this->assertInstanceOf(GridField::class, $field);
+        $field = ElementalEditor::create('ElementalArea', $area);
+        $this->assertInstanceOf(FormField::class, $field);
         $this->assertEquals('ElementalArea', $field->getName());
-
-        // set allowed types, the user should see in the dropdown
-        $editor->setTypes([
-            ElementContent::class
-        ]);
-
-        $field = $editor->getField();
-        $classes = $field->getConfig()->getComponentByType(GridFieldAddNewMultiClass::class)->getClasses($field);
-
-        $this->assertEquals(1, count($classes), 'Only one type available');
-        $this->assertArrayHasKey('DNADesign-Elemental-Models-ElementContent', $classes);
     }
 }
