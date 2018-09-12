4e1267cf8c8ee002237089a86962a63c5f3ba86c
Guy Marriott <guy@scopey.co.nz>
2018-09-12 16:35:01 +1200

    NEW Adding optimistic cache updates to sort query

--- client/src/state/editor/sortBlockMutation.js
+++ client/src/state/editor/sortBlockMutation.js
@@ -1,5 +1,6 @@
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { query as readBlocksQuery, config as readBlocksConfig } from './readBlocksForPageQuery';
 
 // GraphQL query for changing the sort order of blocks
 const mutation = gql`
@@ -15,10 +16,46 @@ mutation SortBlockMutation($blockId:ID!, $afterBlockId:ID!) {
 
 const config = {
   props: ({ mutate, ownProps: { actions } }) => {
-    const handleSortBlock = (blockId, afterBlockId) => mutate({
+    const handleSortBlock = (blockId, afterBlockId, pageId) => mutate({
       variables: {
         blockId,
         afterBlockId,
+      },
+      optimisticResponse: {
+        ID: blockId,
+        __typename: 'Block',
+      },
+      update: store => {
+        const variables = readBlocksConfig.options({ pageId }).variables;
+        const data = store.readQuery({ query: readBlocksQuery, variables });
+
+        // Query returns a deeply nested object. Explicit reconstruction via spreads is too verbose.
+        // This is an alternative, relatively efficient way to deep clone
+        const newData = JSON.parse(JSON.stringify(data));
+        let { edges } = newData.readOnePage.ElementalAreaIfExists.Elements;
+
+        // Find the block we reordered
+        const movedBlockIndex = edges.findIndex(edge => edge.node.ID === blockId);
+        // Keep it
+        const movedBlock = edges[movedBlockIndex];
+        // Remove the moved block
+        edges.splice(movedBlockIndex, 1);
+        // If the target is 0, it's added to the start
+        if (!afterBlockId) {
+          edges.unshift(movedBlock);
+        } else {
+          // Else, find the block we inserted after
+          const targetBlockIndex = edges.findIndex(edge => edge.node.ID === afterBlockId);
+          // Add it back after the target
+          const end = edges.slice(targetBlockIndex + 1);
+          edges = edges.slice(0, targetBlockIndex + 1);
+          edges.push(movedBlock);
+          edges = edges.concat(end);
+        }
+
+        // Add it back to the full result
+        newData.readOnePage.ElementalAreaIfExists.Elements.edges = edges;
+        store.writeQuery({ query: readBlocksQuery, data: newData, variables });
       },
     });
     return {
@@ -28,10 +65,6 @@ const config = {
       },
     };
   },
-  options: {
-    // Refetch versions after mutation is completed
-    refetchQueries: ['ReadBlocksForPage']
-  }
 };
 
 export { mutation, config };
