7dfbfcc302057b77f724090eac67d921e7a63310
Robbie Averill <robbie@silverstripe.com>
2018-09-17 09:53:01 +0200

    Add table name to SQL queries, some documentation and some minor tidy ups

--- src/GraphQL/SortBlockMutationCreator.php
+++ src/GraphQL/SortBlockMutationCreator.php
@@ -4,12 +4,17 @@ namespace DNADesign\Elemental\GraphQL;
 use DNADesign\Elemental\Models\BaseElement;
 use GraphQL\Type\Definition\ResolveInfo;
 use GraphQL\Type\Definition\Type;
+use SilverStripe\Core\Convert;
 use SilverStripe\GraphQL\MutationCreator;
 use SilverStripe\GraphQL\OperationResolver;
 use SilverStripe\ORM\DataObject;
-use SilverStripe\ORM\DB;
 use SilverStripe\ORM\Queries\SQLUpdate;
 
+/**
+ * Given a source block ID and the ID of the block to reorder the source block after, update all affected sort
+ * orders for the block and its siblings. Only the source block will have a new version written, all siblings
+ * will be updated underneath the ORM to avoid this.
+ */
 class SortBlockMutationCreator extends MutationCreator implements OperationResolver
 {
     public function attributes()
@@ -35,6 +40,7 @@ class SortBlockMutationCreator extends MutationCreator implements OperationResol
 
     public function resolve($object, array $args, $context, ResolveInfo $info)
     {
+        /** @var BaseElement $block */
         $block = BaseElement::get_by_id($args['ID']);
 
         if (!$block) {
@@ -52,11 +58,12 @@ class SortBlockMutationCreator extends MutationCreator implements OperationResol
         }
 
         $parentId = $block->ParentID;
-        $blockPosition = $block->Sort;
+        $blockPosition = (int) $block->Sort;
 
         $sortAfterPosition = 0;
         $afterBlockID = $args['AfterBlockID'];
         if ($afterBlockID) {
+            /** @var BaseElement $afterBlock */
             $afterBlock = BaseElement::get_by_id($afterBlockID);
 
             if (!$afterBlock) {
@@ -72,28 +79,31 @@ class SortBlockMutationCreator extends MutationCreator implements OperationResol
                 ));
             }
 
-            $sortAfterPosition = $afterBlock->Sort;
+            $sortAfterPosition = (int) $afterBlock->Sort;
         }
+
+        // We are updating records with SQL queries to avoid the ORM triggering the creation of new versions
+        // for each element that is affected by this reordering.
+        $tableName = Convert::raw2sql(DataObject::getSchema()->tableName(BaseElement::class));
 
         if ($sortAfterPosition < $blockPosition) {
             $operator = '+';
-            $filter = "\"Sort\" > $sortAfterPosition AND \"Sort\" < $blockPosition";
+            $filter = "\"$tableName\".\"Sort\" > $sortAfterPosition AND \"$tableName\".\"Sort\" < $blockPosition";
             $newBlockPosition = $sortAfterPosition + 1;
         } else {
             $operator = '-';
-            $filter = "\"Sort\" <= $sortAfterPosition AND \"Sort\" > $blockPosition";
+            $filter = "\"$tableName\".\"Sort\" <= $sortAfterPosition AND \"$tableName\".\"Sort\" > $blockPosition";
             $newBlockPosition = $sortAfterPosition;
         }
 
-        $table = DataObject::getSchema()->tableName(BaseElement::class);
-
         $query = SQLUpdate::create()
-           ->setTable("\"$table\"")
-           ->assignSQL('"Sort"', "\"Sort\" $operator 1")
-           ->addWhere([$filter, '"ParentID"' => $parentId]);
+           ->setTable("\"$tableName\"")
+           ->assignSQL('"Sort"', "\"$tableName\".\"Sort\" $operator 1")
+           ->addWhere([$filter, "\"$tableName\".\"ParentID\"" => $parentId]);
 
         $query->execute();
 
+        // Now use the ORM to write a new version of the record that we are directly reordering
         $block->Sort = $newBlockPosition;
         $block->write();
 
--- tests/GraphQL/SortBlockMutationCreatorTest.php
+++ tests/GraphQL/SortBlockMutationCreatorTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace DNADesign\Elemental\Tests\GraphQL;
 
 use DNADesign\Elemental\GraphQL\SortBlockMutationCreator;
@@ -12,29 +13,25 @@ class SortBlockMutationCreatorTest extends SapphireTest
     protected static $fixture_file = 'SortBlockMutationCreatorTest.yml';
 
     protected static $extra_dataobjects = [
-        TestElement::class
+        TestElement::class,
     ];
 
-    protected function setUp()
-    {
-        parent::setUp();
-
-        $this->logInWithPermission('ADMIN');
-    }
-
+    /**
+     * Reorders blocks by compounding each result into the next test (rather than isolated sorting tests)
+     */
     public function testSortBlock()
     {
         $this->runMutation(1, 3);
-        $this->assertIdsInOrder([2,3,1,4]);
+        $this->assertIdsInOrder([2, 3, 1, 4]);
 
         $this->runMutation(4, 2);
-        $this->assertIdsInOrder([2,4,3,1]);
+        $this->assertIdsInOrder([2, 4, 3, 1]);
 
         $this->runMutation(1, 0);
-        $this->assertIdsInOrder([1,2,4,3]);
+        $this->assertIdsInOrder([1, 2, 4, 3]);
 
         $this->runMutation(3, 2);
-        $this->assertIdsInOrder([1,2,3,4]);
+        $this->assertIdsInOrder([1, 2, 3, 4]);
     }
 
     protected function assertIdsInOrder($ids)
