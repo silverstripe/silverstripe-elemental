9fe5f054ec1e99705d47780dbe221ee20a766653
Robbie Averill <robbie@silverstripe.com>
2018-10-19 11:16:14 +0200

    Remove externals modification and update webpack-config to 0.13.0

--- webpack.config.js
+++ webpack.config.js
@@ -31,14 +31,7 @@ const config = [
     },
     devtool: (ENV !== 'production') ? 'source-map' : '',
     resolve: resolveJS(ENV, PATHS),
-    externals: Object.assign(
-      {},
-      externalJS(ENV, PATHS),
-      {
-        // @todo remove this once @silverstripe/webpack-config has this updated and published
-        'state/tabs/TabsActions': 'TabsActions',
-      }
-    ),
+    externals: externalJS(ENV, PATHS),
     module: moduleJS(ENV, PATHS),
     plugins: pluginJS(ENV, PATHS),
   },
