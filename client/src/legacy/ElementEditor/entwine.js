e86bad9caa3e7d91571e61d1d04c2f6b12331ae3
Guy Marriott <guy@scopey.co.nz>
2018-11-24 16:10:04 +1300

    FIX Correcting dirty detection & form state issues:
    - Saving page level now destroys the redux-form cache.
    - InlineEditForm now checks if there is existing form state before declaring whether form state should be refetched on mount. Fixes #493

--- client/src/legacy/ElementEditor/entwine.js
+++ client/src/legacy/ElementEditor/entwine.js
@@ -5,6 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { loadComponent } from 'lib/Injector';
 import { getConfig } from 'state/editor/elementConfig';
+import { destroy } from 'redux-form';
 
 /**
  * Uses entwine to inject the HistoryViewer React component into the DOM, when used
@@ -40,7 +41,27 @@ jQuery.entwine('ss', ($) => {
      */
     'from .cms-edit-form': {
       onaftersubmitform() {
+        // After page level saves we need to reload all the blocks from the server. We can remove
+        // this if we can figure out a way to optimistically update the apollo cache. See:
+        // https://github.com/dnadesign/silverstripe-elemental/pull/439#issuecomment-428773370
         window.ss.apolloClient.resetStore();
+
+        // Defer playing with redux store
+        setTimeout(() => {
+          // After the page submit we want to destroy the form values so it's reloaded. We can't
+          // just set the current form state to the original state because some form data is
+          // mutated on the server side.
+          const { store } = window.ss;
+
+          if (!store) {
+            return;
+          }
+
+          // We can introspect the store to find form names in the `element` namespace
+          store.dispatch(destroy(
+            ...Object.keys(store.getState().form.formState.element).map(name => `element.${name}`)
+          ));
+        }, 0);
       }
     },
   });
@@ -70,10 +91,6 @@ jQuery.entwine('ss', ($) => {
         opts = $.extend({}, opts);
         // modify it,
         opts.ignoreFieldSelector += ', .elementalarea :input:not(.element-form-dirty-state)';
-        if (opts.fieldSelector === undefined) {
-          opts.fieldSelector = ':input:not(:button,[type="submit"],[type="search"],.gridstate)';
-        }
-        opts.fieldSelector += ', .element-editor-header';
         // then set the clone as the value on this element
         // (so next call to this method gets this same clone)
         this.setChangeTrackerOptions(opts);
