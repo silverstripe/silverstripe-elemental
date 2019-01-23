7840a58ac40bb66cd1344f7f3324a66a94f0a699
Werner M. Krauß <werner.krauss@netwerkstatt.at>
2019-01-23 14:36:33 +0100

    expose i18n javascript

---
+++ src/Extensions/ElementalLeftAndMainExtension.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DNADesign\Elemental\Extensions;
+
+
+use SilverStripe\Core\Extension;
+use SilverStripe\View\Requirements;
+
+class ElementalLeftAndMainExtension extends Extension
+{
+
+    public function init()
+    {
+        Requirements::add_i18n_javascript('dnadesign/silverstripe-elemental:client/lang');
+    }
+}
