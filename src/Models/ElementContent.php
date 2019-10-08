60d75fd2e8a1ce79fac9dd6a89d2074c614e3c97
bergice <andre.kiste@silverstripe.com>
2019-10-08 20:16:07 +1300

    Do not display records with `hide_in_campaigns` config set to `true` in the `Used On` table of files

--- src/Models/ElementContent.php
+++ src/Models/ElementContent.php
@@ -24,6 +24,14 @@ class ElementContent extends BaseElement
     private static $description = 'HTML text block';
 
     /**
+     * Don't show this model in campaign admin as part of implicit change sets
+     *
+     * @config
+     * @var bool
+     */
+    private static $hide_in_campaigns = true;
+
+    /**
      * Re-title the HTML field to Content
      *
      * {@inheritDoc}
