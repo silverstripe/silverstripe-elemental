4789b34ea93b90e936024c62a47947752ddb7a43
Steve Boyd <emteknetnz@gmail.com>
2024-04-16 19:00:12 +1200

    NEW Validation of fields when inline saving

--- client/src/components/ElementEditor/tests/Content-test.js
+++ client/src/components/ElementEditor/tests/Content-test.js
@@ -3,6 +3,7 @@
 
 import React from 'react';
 import { render } from '@testing-library/react';
+import classnames from 'classnames';
 import { Component as Content } from '../Content';
 
 function makeProps(obj = {}) {
@@ -11,29 +12,98 @@ function makeProps(obj = {}) {
     fileTitle: '',
     content: '',
     previewExpanded: false,
-    InlineEditFormComponent: () => <div className="test-inline-edit-form" />,
+    ensureFormRendered: false,
+    formHasRendered: false,
+    InlineEditFormComponent: (props) => {
+      const className = classnames({
+        'test-inline-edit-form': true,
+        ...props.extraClass
+      });
+      return <div className={className} />;
+    },
     SummaryComponent: () => <div className="test-summary" />,
     ...obj,
   };
 }
 
-test('Content should render the Summary component if the preview is not expanded', () => {
-  const { container } = render(<Content {...makeProps()} />);
-  expect(container.querySelectorAll('.element-editor-content')).toHaveLength(1);
-  expect(container.querySelectorAll('.test-inline-edit-form')).toHaveLength(0);
-  expect(container.querySelectorAll('.test-summary')).toHaveLength(1);
-});
-
-test('Content should render the InlineEditForm component if the preview is expanded', () => {
+test('Content component - previewExpanded is true, ensureFormRendered is false, formHasRendered is false', () => {
   const { container } = render(
     <Content {...makeProps({
-      previewExpanded: true
+      previewExpanded: true,
+      ensureFormRendered: false,
+      formHasRendered: false,
     })}
     />
   );
   expect(container.querySelectorAll('.element-editor-content')).toHaveLength(1);
-  expect(container.querySelectorAll('.test-inline-edit-form')).toHaveLength(1);
   expect(container.querySelectorAll('.test-summary')).toHaveLength(0);
+  expect(container.querySelectorAll('.test-inline-edit-form')).toHaveLength(1);
+  expect(container.querySelectorAll('.element-editor-editform--collapsed')).toHaveLength(0);
+  expect(container.querySelectorAll('.element-editor-editform--rendered-not-visible')).toHaveLength(0);
+});
+
+test('Content component - previewExpanded is true, ensureFormRendered is true, formHasRendered is false', () => {
+  const { container } = render(
+    <Content {...makeProps({
+      previewExpanded: true,
+      ensureFormRendered: true,
+      formHasRendered: false,
+    })}
+    />
+  );
+  expect(container.querySelectorAll('.element-editor-content')).toHaveLength(1);
+  expect(container.querySelectorAll('.test-summary')).toHaveLength(0);
+  expect(container.querySelectorAll('.test-inline-edit-form')).toHaveLength(1);
+  expect(container.querySelectorAll('.element-editor-editform--collapsed')).toHaveLength(0);
+  expect(container.querySelectorAll('.element-editor-editform--rendered-not-visible')).toHaveLength(0);
+});
+
+test('Content component - previewExpanded is true, ensureFormRendered is true, formHasRendered is true', () => {
+  const { container } = render(
+    <Content {...makeProps({
+      previewExpanded: true,
+      ensureFormRendered: true,
+      formHasRendered: true,
+    })}
+    />
+  );
+  expect(container.querySelectorAll('.element-editor-content')).toHaveLength(1);
+  expect(container.querySelectorAll('.test-summary')).toHaveLength(0);
+  expect(container.querySelectorAll('.test-inline-edit-form')).toHaveLength(1);
+  expect(container.querySelectorAll('.element-editor-editform--collapsed')).toHaveLength(0);
+  expect(container.querySelectorAll('.element-editor-editform--rendered-not-visible')).toHaveLength(0);
+});
+
+test('Content component - previewExpanded is false, ensureFormRendered is true, formHasRendered is false', () => {
+  const { container } = render(
+    <Content {...makeProps({
+      previewExpanded: false,
+      ensureFormRendered: true,
+      formHasRendered: false,
+    })}
+    />
+  );
+  expect(container.querySelectorAll('.element-editor-content')).toHaveLength(1);
+  expect(container.querySelectorAll('.test-summary')).toHaveLength(1);
+  expect(container.querySelectorAll('.test-inline-edit-form')).toHaveLength(1);
+  expect(container.querySelectorAll('.element-editor-editform--collapsed')).toHaveLength(1);
+  expect(container.querySelectorAll('.element-editor-editform--rendered-not-visible')).toHaveLength(1);
+});
+
+test('Content component - previewExpanded is false, ensureFormRendered is true, formHasRendered is true', () => {
+  const { container } = render(
+    <Content {...makeProps({
+      previewExpanded: false,
+      ensureFormRendered: true,
+      formHasRendered: true,
+    })}
+    />
+  );
+  expect(container.querySelectorAll('.element-editor-content')).toHaveLength(1);
+  expect(container.querySelectorAll('.test-summary')).toHaveLength(1);
+  expect(container.querySelectorAll('.test-inline-edit-form')).toHaveLength(1);
+  expect(container.querySelectorAll('.element-editor-editform--collapsed')).toHaveLength(1);
+  expect(container.querySelectorAll('.element-editor-editform--rendered-not-visible')).toHaveLength(1);
 });
 
 test('Content renders even when no content or image is provided', () => {
@@ -45,6 +115,6 @@ test('Content renders even when no content or image is provided', () => {
     />
   );
   expect(container.querySelectorAll('.element-editor-content')).toHaveLength(1);
-  expect(container.querySelectorAll('.test-inline-edit-form')).toHaveLength(1);
   expect(container.querySelectorAll('.test-summary')).toHaveLength(0);
+  expect(container.querySelectorAll('.test-inline-edit-form')).toHaveLength(1);
 });
